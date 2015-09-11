2e58dbaa96d02153c922b8b5df3f55e272270d13
nikiforosFreespirit <michele.caci@gmail.com>
2015-09-11 12:29:31 +0200

    Adding concept of Briscola in CardSuit

--- objectmodel/src/game/elements/CardSuit.java
+++ objectmodel/src/game/elements/CardSuit.java
@@ -4,5 +4,20 @@ package game.elements;
  * Created by nikiforos on 23/08/15.
  */
 public enum CardSuit {
-    ORO, COPPE, SPADE, BASTONI
+    ORO, COPPE, SPADE, BASTONI;
+
+    private boolean isBriscola = false;
+
+    public boolean isBriscola() {
+        return isBriscola;
+    }
+
+    public void setAsBriscola() {
+        final CardSuit[] cardSuits = CardSuit.values();
+        for (int i = 0; i < cardSuits.length; i++) {
+            final CardSuit cardSuit = cardSuits[i];
+            cardSuit.isBriscola = this == cardSuit;
+        }
+    }
+
 }
