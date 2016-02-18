086b744b3c94fcd94881b9bc18728eadca2092b3
nikiforos_freespirit <michele.caci@gmail.com>
2016-02-18 21:20:29 +0100

    ScoreCountInfo added

---
+++ objectmodel/src/game/player/info/ScoreCountInfo.java
@@ -0,0 +1,16 @@
+package game.player.info;
+
+/**
+ * Created by nikiforos on 18/02/16.
+ */
+public class ScoreCountInfo {
+    private byte score = 0;
+
+    public byte getScore() {
+        return score;
+    }
+
+    public void setScore(byte score) {
+        this.score = score;
+    }
+}
