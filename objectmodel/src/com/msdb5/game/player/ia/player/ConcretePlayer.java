f4d3c4fbc7a3714a17ac5b0e5379b125dd26bbea
mcaci <michele.caci@amadeus.com>
2016-06-23 13:01:47 +0200

    Junit fix

--- objectmodel/src/com/msdb5/game/player/ia/player/ConcretePlayer.java
+++ objectmodel/src/com/msdb5/game/player/ia/player/ConcretePlayer.java
@@ -17,13 +17,17 @@ public abstract class ConcretePlayer extends Player {
     private static final float CHANCE_TO_FOLD = 0.4F;
 
     @Override
-    public AuctionInfo performAuctionAction(int currentScore) {
+    public AuctionInfo performAuctionAction(int currentAuctionScore) throws AuctionOnScoreOutOfBoundsException {
+        if (currentAuctionScore < AuctionScore.MIN_SCORE || currentAuctionScore >= AuctionScore.MAX_SCORE) {
+            throw new AuctionOnScoreOutOfBoundsException();
+        }
+
         final AuctionInfo auctionInfo = this.getAuctionInfo();
         if (!auctionInfo.getAuctionStatus().hasFolded()) {
             final double randomFlag = Math.random();
             if (randomFlag > CHANCE_TO_FOLD) {
                 auctionInfo.setAuctionStatus(AuctionStatus.IN_AUCTION);
-                auctionInfo.setAuctionScore(chooseNextScore(this.getHand(), currentScore));
+                auctionInfo.setAuctionScore(chooseNextScore(this.getHand(), currentAuctionScore));
             } else {
                 auctionInfo.setAuctionStatus(AuctionStatus.FOLDED);
             }
