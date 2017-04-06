26c75af7f4fde11f7a34481cdb7dc5a9992aae7e
Michele Caci <michele.caci@amadeus.com>
2017-04-06 13:41:36 +0200

    introducing streams

--- objectmodel/src/test/msdb5/game/player/MockClassicPlayer.java
+++ objectmodel/src/test/msdb5/game/player/MockClassicPlayer.java
@@ -1,12 +1,11 @@
 package msdb5.game.player;
 
+import msdb5.game.card.analysis.FixedScaleAnalyzer;
 import msdb5.game.card.set.Hand;
 import msdb5.game.card.Card;
-import msdb5.game.card.analysis.FixedScaleAnalyzer;
 import msdb5.game.card.analysis.ICardAnalyzer;
 
-import java.util.Collection;
-import java.util.Iterator;
+import java.util.stream.Stream;
 
 /**
  * Created by nikiforos on 04/09/15.
@@ -20,15 +19,7 @@ public class MockClassicPlayer extends MockPlayer {
     @Override
     public int evaluateHand(Hand handToEvaluate) {
         ICardAnalyzer cardEvaluator = new FixedScaleAnalyzer();
-        Collection<Card> cards = handToEvaluate.getCardSet();
-        int handValue = 0;
-        Iterator<Card> cardIterator = cards.iterator();
-        while (cardIterator.hasNext()) {
-            int cardValue = cardEvaluator.analyze(cardIterator.next());
-            handValue += cardValue;
-        }
-
-        int weightedHandValue = (int) (handValue / 20.0) * 3;
-        return weightedHandValue;
+        Stream<Card> cards = handToEvaluate.getCardSet().stream();
+        return cards.mapToInt(cardEvaluator::analyze).map((int cardValue) -> (int) (cardValue /20.0) * 3).sum();
     }
 }
