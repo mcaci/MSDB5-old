fa6027d327b6e2806ea5a32df7c72a3e73e98e75
Michele Caci <michele.caci@amadeus.com>
2017-04-06 18:01:59 +0200

    Rewrite class with lambda

--- objectmodel/src/test/msdb5/game/player/MockUnwaveringPlayer.java
+++ objectmodel/src/test/msdb5/game/player/MockUnwaveringPlayer.java
@@ -4,13 +4,13 @@ import msdb5.game.card.set.Hand;
 import msdb5.game.card.Card;
 import msdb5.game.card.CardSuit;
 import msdb5.game.card.analysis.FixedScaleAnalyzer;
-import msdb5.game.card.analysis.ICardAnalyzer;
 import msdb5.game.card.set.analysis.HandAnalysisData;
 import msdb5.game.card.set.analysis.HandAnalyzer;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.function.BiConsumer;
+import java.util.function.ToIntFunction;
+import java.util.stream.Stream;
 
 /**
  * Created by nikiforos on 04/09/15.
@@ -23,63 +23,38 @@ public class MockUnwaveringPlayer extends MockPlayer {
 
     @Override
     public int evaluateHand(Hand handToEvaluate) {
-        HandAnalyzer analyzer = new HandAnalyzer(handToEvaluate);
-        HandAnalysisData analysisData = analyzer.analyze();
-
-        Map<CardSuit, Integer> evaluationPerSuit = suitEvaluated(handToEvaluate);
-
-        int evaluatedMaxScore = evaluationFunction(analysisData, evaluationPerSuit);
-
-        return evaluatedMaxScore;
+        return mapHandToCards().applyAsInt(handToEvaluate);
     }
 
-    private int evaluationFunction(HandAnalysisData analysisData, Map<CardSuit, Integer> evaluationPerSuit) {
-        int value = 60;
+    private ToIntFunction<Hand> mapHandToCards() {
+        return (Hand hand) -> {
+            int value = 60;
+            HandAnalyzer analyzer = new HandAnalyzer(hand);
+            HandAnalysisData analysisData = analyzer.analyze();
+            int suitDensityAdder = Math.round(2 * analysisData.getSuitDensity());
+            int weaknessIndexAdder = 12 / analysisData.getWeaknessIndex();
+            int distanceFromSecondAdder = 3 * analysisData.getDistanceFromSecond();
+            int suitHighestValue = getSuitHighestValue(hand);
+            int total = value + suitDensityAdder + weaknessIndexAdder + distanceFromSecondAdder + suitHighestValue;
 
-        int suitDensityAdder = Math.round(2 * analysisData.getSuitDensity());
-        int weaknessIndexAdder = 12 / analysisData.getWeaknessIndex();
-        int distanceFromSecondAdder = 3 * analysisData.getDistanceFromSecond();
-
-        int suitHighestValue = Collections.max(evaluationPerSuit.values());
-
-        System.out.println(analysisData);
-        System.out.println(evaluationPerSuit);
-
-        int total = value + suitDensityAdder + weaknessIndexAdder + distanceFromSecondAdder + suitHighestValue;
-
-        return Math.round(0.85F * total);
+            return Math.round(0.85F * total);
+        };
     }
 
-    private Map<CardSuit, Integer> suitEvaluated(Hand handToEvaluate) {
-        Map<CardSuit, Integer> sumSuitEvaluation = getCardSuitIntegerMap(handToEvaluate);
-        weightValuesInMap(sumSuitEvaluation);
-        return sumSuitEvaluation;
+    private int getSuitHighestValue(Hand hand) {
+        BiConsumer<Map<CardSuit, Integer>, Card> cardSuitMapper = getMapCardBiConsumer();
+        Stream<Card> cardStream = hand.getCardSet().stream();
+        Optional<Integer> max = cardStream.collect(HashMap::new, cardSuitMapper, Map::putAll).values().stream().max(Integer::compareTo);
+        return (max.isPresent() ? max.get() : 0);
     }
 
-    private void weightValuesInMap(Map<CardSuit, Integer> sumSuitEvaluation) {
-        for (Map.Entry<CardSuit, Integer> suitValue :
-                sumSuitEvaluation.entrySet()) {
-            CardSuit cardSuit = suitValue.getKey();
-            int currentValue = suitValue.getValue();
-            int weightedValue = (int) (currentValue / 20.0) * 3;
-            sumSuitEvaluation.put(cardSuit, weightedValue);
-        }
-    }
-
-    private Map<CardSuit, Integer> getCardSuitIntegerMap(Hand handToEvaluate) {
-        Map<CardSuit, Integer> sumSuitEvaluation = new HashMap<>();
-
-        ICardAnalyzer cardAnalyzer = new FixedScaleAnalyzer();
-
-        for (Card card :
-                handToEvaluate.getCardSet()) {
+    private BiConsumer<Map<CardSuit, Integer>, Card> getMapCardBiConsumer() {
+        return (Map<CardSuit, Integer> map, Card card) -> {
             CardSuit cardSuit = card.getCardSuit();
-            Integer currentValue = sumSuitEvaluation.getOrDefault(cardSuit, 0);
-            int evaluateCard = cardAnalyzer.analyze(card);
-            sumSuitEvaluation.put(cardSuit, currentValue + evaluateCard);
-        }
-        return sumSuitEvaluation;
+            Integer currentValue = map.getOrDefault(cardSuit, 0);
+            int cardEvaluation = new FixedScaleAnalyzer().analyze(card);
+            int finalValue = (int) ((currentValue + cardEvaluation) / 20.0) * 3;
+            map.put(cardSuit, finalValue);
+        };
     }
-
-
 }
