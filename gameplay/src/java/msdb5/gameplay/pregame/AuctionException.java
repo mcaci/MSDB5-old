a4a438ca3138e829f06d5aeeaf0e79a6e09b0b7a
nikiforosFreespirit <michele.caci@gmail.com>
2017-04-23 16:38:26 +0200

    AuctionException creation

---
+++ gameplay/src/java/msdb5/gameplay/pregame/AuctionException.java
@@ -0,0 +1,10 @@
+package msdb5.gameplay.pregame;
+
+/**
+ * Created by nikiforos on 23/04/17.
+ */
+public class AuctionException extends Exception {
+    public AuctionException(Exception e) {
+        super(e);
+    }
+}
