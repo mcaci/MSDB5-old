dd01d9a7a7a66e639281fc28ddf6b64d171831ee
nikiforosFreespirit <michele.caci@gmail.com>
2017-05-06 18:51:21 +0200

    Removing general cardSetFactory

--- objectmodel/src/java/msdb5/game/card/set/DeckFactory.java
+++ objectmodel/src/java/msdb5/game/card/set/DeckFactory.java
@@ -1,20 +1,34 @@
 package msdb5.game.card.set;
 
 import msdb5.game.card.Card;
+import msdb5.game.card.CardNumber;
+import msdb5.game.card.CardSuit;
 
 import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.Random;
+import java.util.function.IntFunction;
+import java.util.stream.Collectors;
 
 /**
  * Created by nikiforos on 08/09/15.
  */
-public class DeckFactory extends CardSetFactory {
+public class DeckFactory {
 
-    public DeckFactory() {
-        super(Deck.DEFAULT_DECK_SIZE);
-    }
+    public static final int DECK_SIZE = Deck.DEFAULT_DECK_SIZE;
 
-    @Override
     public Deck create() {
-        return new Deck(this.createCardSet(ArrayDeque<Card>::new));
+        final IntFunction<Card> idToCardFunction = id -> {
+            CardNumber[] numbers = CardNumber.values();
+            CardSuit[] suits = CardSuit.values();
+            final CardNumber number = numbers[id % numbers.length];
+            final CardSuit suit = suits[id / numbers.length];
+            return new Card(number, suit);
+        };
+
+        Queue<Card> deckContainer = new Random().ints(0, DECK_SIZE).distinct().limit(DECK_SIZE).
+                mapToObj(idToCardFunction).collect(Collectors.toCollection(ArrayDeque<Card>::new));
+
+        return new Deck(deckContainer);
     }
 }
--- objectmodel/src/java/msdb5/game/card/set/EmptyCardSetFactory.java
+++ objectmodel/src/java/msdb5/game/card/set/EmptyCardSetFactory.java
@@ -1,10 +1,18 @@
 package msdb5.game.card.set;
 
+import msdb5.game.card.Card;
+
+import java.util.Collection;
+import java.util.function.Supplier;
+
 /**
  * Created by nikiforos on 08/09/15.
  */
-abstract class EmptyCardSetFactory extends CardSetFactory {
-    EmptyCardSetFactory() {
-        super(0);
+abstract class EmptyCardSetFactory {
+
+    public abstract CardSet<? extends Collection<Card>> create();
+
+    <T extends Collection<Card>> T createCardSet(Supplier<T> containerSupplier) {
+        return containerSupplier.get();
     }
 }
