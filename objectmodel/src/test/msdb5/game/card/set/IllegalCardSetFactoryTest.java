3bb9d7f5221ff360e3575a36ec37a1c660cba9d1
nikiforosFreespirit <michele.caci@gmail.com>
2017-05-06 10:25:31 +0200

    Card set Factory enhancements

--- objectmodel/src/test/msdb5/game/card/set/IllegalCardSetFactoryTest.java
+++ objectmodel/src/test/msdb5/game/card/set/IllegalCardSetFactoryTest.java
@@ -2,8 +2,6 @@ package msdb5.game.card.set;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
-
 /**
  * Created by mcaci on 4/12/17.
  */
@@ -11,7 +9,7 @@ public class IllegalCardSetFactoryTest {
 
     @Test(expected = IllegalArgumentException.class)
     public void testIllegalSize() throws Exception {
-        new CardSetFactory(ArrayList::new,-1) {
+        new CardSetFactory(-1) {
             @Override
             public CardSet create() {
                 return null;
