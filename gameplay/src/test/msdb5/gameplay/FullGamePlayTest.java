3d04a441b7da1bd8c3c2a635d3dd95a7e6531bb6
nikiforosFreespirit <michele.caci@gmail.com>
2017-06-14 11:10:31 +0200

    Update the full game play test

--- gameplay/src/test/msdb5/gameplay/FullGamePlayTest.java
+++ gameplay/src/test/msdb5/gameplay/FullGamePlayTest.java
@@ -1,59 +1,54 @@
 package msdb5.gameplay;
 
+import msdb5.game.player.Player;
+import msdb5.game.table.GameTable;
+import msdb5.game.table.GameTableFactory;
+import msdb5.gameplay.endgame.ScoreCountRoulette;
+import msdb5.gameplay.ingame.SimpleIngameRoulette;
+import msdb5.gameplay.player.TestPlayerForGamePlayer;
+import msdb5.gameplay.pregame.AuctionRoulette;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+
 /**
  * Created by nikiforos on 08/02/16.
  */
 public class FullGamePlayTest {
 
-//    private GameTable inputOutputGameTable;
-//    private String stepName;
-//
-//    @Before
-//    public void setUp() throws Exception {
-//        Player[] testPlayers = {new Ambiguo(), new Buonagno(), new CampioneDecaduto(), new Dubbioso(), new Rialzatore()};
-//        inputOutputGameTable = new GameTableFactory(true).create(testPlayers);
-//        stepName = "";
-//    }
-//
-//    @After
-//    public void tearDown() throws Exception {
-//        System.out.println("FullGamePlayTest: step " + stepName);
-//        System.out.println(inputOutputGameTable);
-//    }
-//
-//    @Test
-//    public void step1() throws Exception {
-//        stepName = "Preparation (auction, choosing suit, managing side deck, choosing number/card)";
-//        AuctionRoulette auctionRouletteTest = new AuctionRoulette();
-//        auctionRouletteTest.executeOn(inputOutputGameTable);
-//
-//        Player auctionWinner = inputOutputGameTable.getGameTableInfo().getAuctionWinner();
-//        assertNotNull(auctionWinner);
-//        Card anionCard = inputOutputGameTable.getGameTableInfo().getPairedPlayerCard();
-//        assertNotNull(anionCard);
-//    }
-//
-//    @Test
-//    public void step2() throws Exception {
-//        step1();
-//        stepName = "Hostilities (play all rounds until all card are played)";
-//        HostilitiesRoulette hostilitiesRouletteTest = new HostilitiesRoulette();
-//        hostilitiesRouletteTest.executeOn(inputOutputGameTable);
-//        for (Player player : inputOutputGameTable.getPlayers()) {
-//            assertEquals(0, player.getHand().size());
-//        }
-//    }
-//
-//    @Test
-//    public void step3() throws Exception {
-//        step2();
-//        stepName = "Score count (count all points and determine winners)";
-//        ScoreCountRoulette scoreCountRouletteTest = new ScoreCountRoulette();
-//        scoreCountRouletteTest.executeOn(inputOutputGameTable);
-//        int sumOfScores = 0;
-//        for (Player player : inputOutputGameTable.getPlayers()) {
-//            sumOfScores += player.tellScore();
-//        }
-//        assertEquals(120, sumOfScores);
-//    }
+    private GameTable inputOutputGameTable;
+
+    @Before
+    public void setUp() throws Exception {
+        Player[] testPlayers = {new TestPlayerForGamePlayer(1), new TestPlayerForGamePlayer(2), new TestPlayerForGamePlayer(3), new TestPlayerForGamePlayer(4), new TestPlayerForGamePlayer(5)};
+        inputOutputGameTable = new GameTableFactory(false).create(testPlayers);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        System.out.println(inputOutputGameTable);
+    }
+
+    @Test
+    public void execute() throws Exception {
+        GameRoulette gameRoulette;
+
+        // step1
+        gameRoulette = new AuctionRoulette();
+        gameRoulette.executeOn(inputOutputGameTable);
+
+        // step2
+        gameRoulette = new SimpleIngameRoulette();
+        gameRoulette.executeOn(inputOutputGameTable);
+
+        // step3
+        gameRoulette = new ScoreCountRoulette();
+        gameRoulette.executeOn(inputOutputGameTable);
+
+        assertNotNull(inputOutputGameTable);
+
+    }
+
 }
